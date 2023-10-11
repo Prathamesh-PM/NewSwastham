0662f576357657663cdcbc95556368d1b446ac78
Prathamesh-PM <prathameshm5900@gmail.com>
2023-10-11 12:26:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Swastham
+Swastham is a web based application build for patients easily interact with doctors and consult from the best doctors.
---
+++ js/loginscript.js
@@ -0,0 +1,45 @@
+const wrapper = document.querySelector('.wrapper');
+const signUpLink = document.querySelector('.signUp-link');
+const signInLink = document.querySelector('.signIn-link');
+
+signUpLink.addEventListener('click', () => {
+    wrapper.classList.add('animate-signIn');
+    wrapper.classList.remove('animate-signUp');
+});
+
+signInLink.addEventListener('click', () => {
+    wrapper.classList.add('animate-signUp');
+    wrapper.classList.remove('animate-signIn');
+});
+
+const express = require("express");
+const path = require("path")
+const mysql = require("mysql");
+const app = express();
+const dotenv = require("dotenv").config();
+const cookieParser = require("cookie-parser");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(cookieParser());
+app.set('view engine', 'html');
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("MYSQL CONNECTED")
+    }
+})
+// Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5000)
